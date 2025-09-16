4b0ae5accc65ffee78645012c4cf9f0ae1393195
William <167653827+WilliamBl7417@users.noreply.github.com>
2025-09-16 10:09:59 +0200

    Adding Components to PlayerClass
    Add camera components
    Add Input
    Add movement
    add looking
    add click
    add jump

--- Source/MyWorld/Private/Player/WorldPlayer.cpp
+++ Source/MyWorld/Private/Player/WorldPlayer.cpp
@@ -1,9 +1,22 @@
 #include "Player/WorldPlayer.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 AWorldPlayer::AWorldPlayer()
 {
  	
 	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom);
 
 }
 
@@ -11,6 +24,15 @@ AWorldPlayer::AWorldPlayer()
 void AWorldPlayer::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(PlayerMappingContext, 0);
+		}
+
+	}
 	
 }
 
@@ -24,6 +46,57 @@ void AWorldPlayer::Tick(float DeltaTime)
 void AWorldPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhancedInputComp = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComp->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AWorldPlayer::MoveEvent);
+		EnhancedInputComp->BindAction(LookAction, ETriggerEvent::Triggered, this, &AWorldPlayer::LookEvent);
+		EnhancedInputComp->BindAction(InteractAction, ETriggerEvent::Triggered, this, &AWorldPlayer::InteractEvent);
+		EnhancedInputComp->BindAction(JumpAction, ETriggerEvent::Triggered, this, &AWorldPlayer::JumpEvent);
+
+	}
 
 }
+
+void AWorldPlayer::MoveEvent(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void AWorldPlayer::LookEvent(const FInputActionValue& Value)
+{
+	if (Controller != nullptr)
+	{
+		const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+		// Add yaw (left/right) input
+		AddControllerYawInput(LookAxisVector.X * LookSensitivity);
+
+		// Add pitch (up/down) input
+		AddControllerPitchInput(LookAxisVector.Y * LookSensitivity);
+	}
+}
+
+void AWorldPlayer::InteractEvent(const FInputActionValue& Value)
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Interact Event Triggered"));
+
+	}
+}
+
+void AWorldPlayer::JumpEvent(const FInputActionValue& Value)
+{
+	if(GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Jump Event Triggered"));
+	}
+}
+
 
--- Source/MyWorld/Public/Player/WorldPlayer.h
+++ Source/MyWorld/Public/Player/WorldPlayer.h
@@ -2,7 +2,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include <EnhancedInputLibrary.h>
 #include "WorldPlayer.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class MYWORLD_API AWorldPlayer : public ACharacter
@@ -13,14 +19,45 @@ public:
 
 	AWorldPlayer();
 
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/* Inputs // IMC Implementation*/
+
+	void MoveEvent(const FInputActionValue& Value);
+	void LookEvent(const FInputActionValue& Value);
+	void InteractEvent(const FInputActionValue& Value);
+	void JumpEvent(const FInputActionValue& Value);
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* PlayerMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* InteractAction;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* JumpAction;
+
+	/* Camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+
+	UPROPERTY(EditAnywhere, Category = "Player Properties|Camera")
+	float LookSensitivity = 1.0f;
+
+
+
 protected:
 
 	virtual void BeginPlay() override;
 
 public:	
-
-	virtual void Tick(float DeltaTime) override;
-
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
